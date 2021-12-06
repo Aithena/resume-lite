57b5c8a210e9da6661d8a35204f9e0c7ddc0d4f9
liqingyun <liqingyun.chn@qq.com>
2021-12-06 22:11:59 +0800

    1

--- index.js
+++
@@ -1,7 +0,0 @@
-const schedule = require('node-schedule')
-const rule = new schedule.RecurrenceRule()
-rule.second = 10
-
-const j = schedule.scheduleJob (rule, function() {
-  console.log("每分钟5s的时候提示一下")
-})
--- test/01/index.js
+++ test/01/index.js
@@ -3,5 +3,3 @@ const a2 = require('./02')
 
 console.log(a1)
 a1.add()
-console.log('----------')
-console.log(a2)
